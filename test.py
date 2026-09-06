4e12fe15d97fa8385712cac147b1a8e8bd718072
chanetauto <chandansahu23@gmail.com>
2026-09-07 01:18:04 +0530

    commit bulk

--- test.py
+++
@@ -1,3 +0,0 @@
-ansible all -i 192.168.0.40, -c ansible.netcommon.network_cli -u admin -k -m cisco.ios.ios_facts -e ansible_network_os=cisco.ios.ios
-
-ansible all -i 192.168.0.41, -c ansible.netcommon.network_cli -u admin -k -m cisco.ios.ios_command -e ansible_network_os=cisco.ios.ios -a "commands='show ip int brief'"
